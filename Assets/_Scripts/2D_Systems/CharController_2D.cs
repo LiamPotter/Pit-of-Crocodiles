0cdc802c69fa184c5e047c425e5eae5694d027f7
Liam Potter <liamp62@gmail.com>
2016-11-14 09:55:35 +1000

    Added all the stuff
    - It's great, there's things!

---
+++ Assets/_Scripts/2D_Systems/CharController_2D.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController_2D : MonoBehaviour {
+
+    public static CharacterController CharacterController;
+
+    public static CharController_2D Instance;
+
+    public static InputHandler_2D InputHandler;
+
+	void Awake ()
+    {
+        try
+        {
+            CharacterController = GetComponent<CharacterController>();
+            InputHandler = GetComponent<InputHandler_2D>();
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError("The Player needs to have a CharacterController and InputHandler_2D component!");
+            throw;
+        }
+        Instance = this;
+	}
+	
+	void Update ()
+    {
+        if (Camera.main == null)
+            return;
+        GetLocomotionInput();
+	}
+    void GetLocomotionInput()
+    {
+        InputHandler.GetInput();
+        Debug.Log(InputHandler.horizontalValue);
+    }
+}
---
+++ Assets/_Scripts/2D_Systems/InputHandler_2D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+public class InputHandler_2D : MonoBehaviour {
+
+    public static Player player;
+
+    public float horizontalValue;
+
+	void Awake ()
+    {
+        player = ReInput.players.GetPlayer(0);
+	}
+    public void GetInput()
+    {
+        horizontalValue = player.GetAxis("Horizontal");
+    }
+
+}
